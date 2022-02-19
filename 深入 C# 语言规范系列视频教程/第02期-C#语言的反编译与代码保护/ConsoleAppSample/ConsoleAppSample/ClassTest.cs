525fa382343f3ce7809c42629764d871adaf452c
zhengflwr <zhengflwr@outlook.com>
2022-02-19 21:01:08 +0800

    深入 C# 语言规范系列视频教程

---
+++ 深入 C# 语言规范系列视频教程/第02期-C#语言的反编译与代码保护/ConsoleAppSample/ConsoleAppSample/ClassTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSample
+{
+    internal class ClassTest
+    {
+        //static void Main(string[] args)
+        //{
+        //    string[] strArr = args;
+        //    int i = 0;
+        //    int @int = 2;
+        //    Console.WriteLine("Hellow World ");
+        //    Console.WriteLine(@int.ToString());
+
+        //}
+    }
+}
---
+++ 深入 C# 语言规范系列视频教程/第02期-C#语言的反编译与代码保护/ConsoleAppSample/ConsoleAppSample/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string[] strArr = args;
+int i = 0;
+int @int = 2;
+ClassLibrary1.Class1 vb = new ClassLibrary1.Class1();
+double a = vb.GetArea(2, 3);
+Console.WriteLine("Hellow World ");
+Console.WriteLine(@int.ToString());
+Console.WriteLine("调用vb方法：" + a.ToString());
